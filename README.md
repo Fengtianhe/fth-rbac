2326c53beff9360de6c44f9b68e56b933bb0aae7
fengtianhe <545704061@qq.com>
2020-11-10 23:27:02 +0800

    init

---
+++ README.md
@@ -0,0 +1,9 @@
+## 项目目录说明
+
+* fth-rbac-server-java 		fth-rbac后端服务(java版本)
+* fth-rbac-server-admin 	fth-rbac后台页面
+* fth-rbac-client-npm 		fth-rbac SDK(npm版本)
+* fth-rbac-client-java		fth-rbac SDK(java版本)
+* fth-rbac-client-php 		fth-rbac SDK(php版本)
+
+***
---
+++ fth-rbac-server-java/src/main/java/com/fth/rbac/server/FthRbacServerApplication.java
@@ -0,0 +1,17 @@
+package com.fth.rbac.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 启动类
+ *
+ * @author fengtianhe
+ */
+
+@SpringBootApplication
+public class FthRbacServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(FthRbacServerApplication.class, args);
+    }
+}
