f5da268718b288a36218d87e3ce0c54e2b8bee97
fengtianhe <545704061@qq.com>
2020-12-03 16:15:16 +0800

    更改用户信息

--- fth-rbac-server-admin/src/web/store/modules/user.js
+++ fth-rbac-server-admin/src/web/store/modules/user.js
@@ -3,31 +3,57 @@ import service from '@common/utils/request';
 const user = {
   state: {
     token: '',
-    info: {}
+    info: {
+      id: '',
+      username: '',
+      nickname: '',
+      status: '',
+      createdAt: ''
+    }
   },
   mutations: {
     SET_TOKEN: function (state, token) {
       state.token = token;
     },
     CLEAR_TOKEN: function (state) {
-      state.token = ''
+      state.token = '';
     },
     SET_USER_INFO: async function (state) {
       if (state.token) {
         const response = await service.get('/api/sys/user/info');
         state.info = response.data;
       } else {
-        this.CLEAR_TOKEN(state)
+        this.CLEAR_TOKEN(state);
       }
     }
   },
   actions: {
-    LOGOUT ({commit}) {
+    LOGOUT({ commit }) {
       return new Promise((resolve) => {
         commit('CLEAR_TOKEN');
-        commit('CLEAR_MENUS')
+        commit('CLEAR_MENUS');
         resolve();
       });
+    },
+    async UPT_USER({ commit, state }, payload) {
+      console.log(payload);
+      const keys = Object.keys(payload);
+      const { info } = state;
+      const uptData = {};
+      let upt = false;
+      // 对比数据，有变化才更新
+      for (let k of keys) {
+        if (payload[k] && payload[k] !== info[k]) {
+          uptData[k] = payload[k];
+          upt = true;
+        }
+      }
+      // 没有数据变化
+      if (!upt) {
+        return;
+      }
+      await service.put('/api/sys/user', uptData);
+      commit('SET_USER_INFO');
     }
   }
 };
--- fth-rbac-server-admin/src/web/views/layout/components/Navbar.vue
+++ fth-rbac-server-admin/src/web/views/layout/components/Navbar.vue
@@ -5,47 +5,56 @@
       <el-tooltip effect="dark" content="全屏" placement="bottom">
         <screenfull class="screenfull right-menu-item"></screenfull>
       </el-tooltip>
-      <el-dropdown class="avatar-container" trigger="click">
+      <el-dropdown class="avatar-container" trigger="click" @command="handleUserDropdown">
         <div class="avatar-wrapper">
           <!--          <img class="user-avatar" :src="avatar+'?imageView2/1/w/80/h/80'">-->
-          <div class="username-avatar">{{user.username | substr(1)}}</div>
+          <div class="username-avatar">{{user.nickname || user.username | substr(1)}}</div>
           <i class="el-icon-caret-bottom"></i>
         </div>
         <el-dropdown-menu class="user-dropdown" slot="dropdown">
-          <el-dropdown-item divided>
-            <span @click="userDetailDialog" style="display:block;">编辑资料</span>
-          </el-dropdown-item>
-          <el-dropdown-item divided>
-            <span @click="logout" style="display:block;">退出登录</span>
+          <el-dropdown-item v-for="dp in userDropdown" :key="dp.command" :command="dp.command" :divided="dp.divided">
+            {{dp.label}}
           </el-dropdown-item>
         </el-dropdown-menu>
       </el-dropdown>
     </el-menu>
-    <el-dialog class="modify-user-dialog" title="编辑个人资料" :visible.sync="dialogFormVisible" center>
-      <el-form :model="userForm" :rules="rules" ref="userForm" label-width="80px">
-        <el-form-item label="用户名" prop="username">
-          <el-input v-model="userForm.username" auto-complete="off" maxlength="32" :disabled="true"></el-input>
-        </el-form-item>
-        <el-form-item label="姓名" prop="realName">
-          <el-input v-model="userForm.realName" auto-complete="off" maxlength="180"
-                    placeholder="请输入真实姓名"></el-input>
-        </el-form-item>
-        <el-form-item label="手机号" prop="phone">
-          <el-input v-model="userForm.phone" auto-complete="off" maxlength="16"
-                    placeholder="请输入手机号"></el-input>
-        </el-form-item>
 
-        <el-form-item label="密码" prop="pass">
-          <el-input type="password" v-model="userForm.password" auto-complete="off"></el-input>
+    <!--  更新个人资料  -->
+    <el-dialog class="modify-user-dialog" :visible.sync="dialogFormVisible">
+      <div class="userform-avatar">
+        <div class="username-avatar">{{user.nickname || user.username | substr(1)}}</div>
+        <div class="username">{{user.username}}</div>
+      </div>
+      <el-form :model="userForm" ref="userForm" label-width="80px" class="userinfo-form">
+        <el-form-item label="姓名">
+          <el-input v-model="userForm.nickname" maxlength="180" placeholder="请输入真实姓名"></el-input>
         </el-form-item>
-        <el-form-item label="确认密码" prop="checkPass">
-          <el-input type="password" v-model="userForm.checkPass" auto-complete="off"></el-input>
+        <el-form-item label="手机号">
+          <el-input v-model="userForm.phone" maxlength="11" placeholder="请输入手机号"></el-input>
         </el-form-item>
-
       </el-form>
       <div slot="footer" class="dialog-footer">
         <el-button @click="dialogFormVisible = false">取 消</el-button>
         <el-button type="primary" @click="updateUserForm">提 交</el-button>
+      </div>
+    </el-dialog>
+
+    <!--  修改密码  -->
+    <el-dialog class="modify-user-dialog" title="修改密码" :visible.sync="uptPwdDialog" center>
+      <el-form :model="uptPwdForm" :rules="uptPwdRules" ref="uptPwdForm" label-width="80px">
+        <el-form-item label="当前密码" prop="password">
+          <el-input v-model="uptPwdForm.username" placeholder="请输入当前密码"></el-input>
+        </el-form-item>
+        <el-form-item label="新密码" prop="newPass">
+          <el-input v-model="uptPwdForm.realName" placeholder="请输入新密码"></el-input>
+        </el-form-item>
+        <el-form-item label="重新输入" prop="checkPass">
+          <el-input v-model="uptPwdForm.phone" placeholder="请输入重新输入新密码"></el-input>
+        </el-form-item>
+      </el-form>
+      <div slot="footer" class="dialog-footer">
+        <el-button @click="uptPwdDialog = false">取 消</el-button>
+        <el-button type="primary" @click="uptPwd()">提 交</el-button>
       </div>
     </el-dialog>
   </div>
@@ -67,8 +76,8 @@ export default {
       if (value === '') {
         callback(new Error('请输入密码'));
       } else {
-        if (this.userForm.checkPass !== '') {
-          this.$refs.userForm.validateField('checkPass');
+        if (this.uptPwdForm.checkPass !== '') {
+          this.$refs.uptPwdForm.validateField('checkPass');
         }
         callback();
       }
@@ -76,78 +85,84 @@ export default {
     var validatePass2 = (rule, value, callback) => {
       if (value === '') {
         callback(new Error('请再次输入密码'));
-      } else if (value !== this.userForm.password) {
+      } else if (value !== this.uptPwdForm.newPass) {
         callback(new Error('两次输入密码不一致!'));
       } else {
         callback();
       }
     };
     return {
+      userDropdown: [
+        { command: 'UPDATE', label: '编辑资料', divided: false },
+        { command: 'PASSWORD', label: '修改密码', divided: false },
+        { command: 'LOGOUT', label: '退出登录', divided: true }
+      ],
       dialogFormVisible: false,
       userForm: {
-        id: null,
-        username: '',
-        realName: '',
-        phone: '',
-        userType: null,
+        nickname: '',
+        phone: ''
+      },
+      uptPwdDialog: false,
+      uptPwdForm: {
         password: '',
+        newPass: '',
         checkPass: ''
       },
-      rules: {
-        username: [{ required: true, message: '请输入用户名', trigger: 'blur' }],
-        realName: [{ required: true, message: '请输入真实姓名', trigger: 'blur' }],
-        phone: [{ required: true, message: '手机号不能为空' }],
-        pass: [
+      uptPwdRules: {
+        password: [
+          { required: true, validator: validatePass, trigger: 'blur' }
+        ],
+        newPass: [
           { required: true, validator: validatePass, trigger: 'blur' }
         ],
         checkPass: [
           { required: true, validator: validatePass2, trigger: 'blur' }
-        ],
-      },
+        ]
+      }
     };
   },
   computed: {
     ...mapGetters([
-      'sidebar',
       'user'
     ])
   },
   methods: {
-    toggleSideBar() {
-      this.$store.dispatch('ToggleSideBar');
+    handleUserDropdown(command) {
+      console.log(command);
+      switch (command) {
+        case 'UPDATE':
+          this.userDetailDialog();
+          break;
+        case 'PASSWORD':
+          this.$refs['uptPwdForm'].resetField();
+          this.uptPwdDialog();
+          break;
+        case 'LOGOUT':
+          this.logout();
+          break;
+        default:
+          break;
+      }
     },
     logout() {
       this.$store.dispatch('LOGOUT');
-      this.$router.push({path: '/login'});
+      this.$router.push({ path: '/login' });
     },
     userDetailDialog() {
       this.dialogFormVisible = true;
-      const { username } = this.user;
-      this.userForm.username = username;
+      const { nickname, phone } = this.user;
+      this.userForm = {
+        nickname,
+        phone,
+      };
     },
     updateUserForm() {
-      // update(this.userForm).then(res => {
-      //     const { code, msg } = res;
-      //     if (code === 200) {
-      //       this.$message({
-      //         message: '修改成功',
-      //         type: 'success'
-      //       });
-      //       this.dialogFormVisible = false;
-      //     } else {
-      //       this.$message({
-      //         message: msg,
-      //         type: 'error'
-      //       });
-      //     }
-      //   }
-      // ).catch(function (err) {
-      //   console.log(err.message);
-      // });
+      this.$store.dispatch('UPT_USER', this.userForm).then(() => {
+        this.dialogFormVisible = false;
+      });
     }
   }
-}
-;
+};
 </script>
 
 <style lang="less" scoped>
@@ -183,17 +198,6 @@ export default {
         align-items: center;
         position: relative;
 
-        .username-avatar {
-          background: #304156;
-          color: #fff;
-          text-transform: uppercase;
-          border-radius: 50%;
-          width: 30px;
-          height: 30px;
-          text-align: center;
-          line-height: 30px;
-        }
-
         .user-avatar {
           width: 40px;
           height: 40px;
@@ -210,9 +214,38 @@ export default {
     }
   }
 
+  .username-avatar {
+    background: #304156;
+    color: #fff;
+    text-transform: uppercase;
+    border-radius: 50%;
+    width: 30px;
+    height: 30px;
+    text-align: center;
+    line-height: 30px;
+  }
+
   .modify-user-dialog {
-    .el-input {
-      width: 80%;
+
+    .userform-avatar {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+
+      .username-avatar {
+        width: 70px;
+        height: 70px;
+        font-size: 30px;
+        line-height: 70px;
+      }
+
+      .username {
+        margin-top: 20px;
+      }
+    }
+
+    .userinfo-form {
+      margin-top: 20px;
     }
   }
 </style>
